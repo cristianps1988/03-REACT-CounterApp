7acd703a26aa6b748074ef397031086cf8934e78
Cristianps1988 <cristianps_1@hotmail.com>
2023-05-26 06:52:21 -0500

    props, proptypes lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-    return <h1>Hola mundo</h1>
+import PropTypes from 'prop-types'
+
+const nombre = 'Cristian'
+
+const saludo = (nombre) => {
+    return `Hola, mi nombre es ${nombre}`
+}
+
+function App({ cargo, edad, title }) {
+
+    return (
+        <>
+            <h1>{title}</h1>
+            <p>{saludo(nombre)}</p>
+            <p>Cargo: {cargo}</p>
+            <p>Edad: {edad}</p>
+        </>
+    )
+}
+
+App.propTypes = {
+    title: PropTypes.string.isRequired,
+    edad: PropTypes.number.isRequired
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from './App'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <App cargo='Docente' edad={35} title='Hola mundo' />
     </React.StrictMode>
 )
